4e883ce1e95663e73b592ea9ce486cd984bfcfb6
pramod-code <43893075+pramod-code@users.noreply.github.com>
2018-10-21 11:05:16 -0400

    Create CountryList.py

---
+++ python/2018-Oct-Intermediate/CountryList.py
@@ -0,0 +1,10 @@
+#Country List
+nameList = []
+print ("Enter 5 Countries (press the Enter key  after each ):")
+for i in range(5):
+    name = input()
+    nameList.append(name)
+
+print ("The names are:", nameList)
+
+# Sort
